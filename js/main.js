2259508861c90cd40feb0c1708513c619364bc2e
asdas <95152341+1213212132@users.noreply.github.com>
2023-09-07 11:43:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -88,6 +88,28 @@ $(function () {
   });
 
 
+  //#web 스와이퍼
+
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    loop: true,
+    mousewheel: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    // autoHeight: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
 
   //#web 스와이퍼
 
